45290cfac6ce107df5f84dbb682dcf4ead0d024d
Hoang Nguyen <nbhoangit@gmail.com>
2025-06-27 14:02:38 +0200

    feat: update pc config

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
   outputs =
     { nixpkgs, self, ... }@inputs:
     let
-      username = "frostphoenix";
+      username = "hoang";
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
