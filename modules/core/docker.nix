b9b96c5de5c1c77a5785faa3887da320ad22a704
Hoang Nguyen <nbhoangit@gmail.com>
2025-06-27 16:31:38 +0200

    feat: add docker podman

---
+++ modules/core/docker.nix
@@ -0,0 +1,28 @@
+{ user, username, pkgs, ... }: {
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    docker
+    podman
+    podman-compose
+  ];
+
+  users.users.${username}.extraGroups = [ "docker" ];
+
+  virtualisation.docker = {
+    enable = true;
+  };
+
+  virtualisation.containers = {
+    enable = true;
+    registries.search = [ "docker.io" "quay.io" ];
+    policy = {
+      default = [ { type = "insecureAcceptAnything"; } ];
+      transports = {
+        docker-daemon = {
+          "" = [ { type = "insecureAcceptAnything"; } ];
+        };
+      };
+    };
+  };
+}
