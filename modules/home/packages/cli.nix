139e02d0190339963bb27210e15ffb03b694b255
Hoang Nguyen <nbhoangit@gmail.com>
2025-06-27 16:25:42 +0200

    feat: add stow for manage dotfiles

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+
+    stow
+
     ## Better core utils
     duf                               # disk information
     eza                               # ls replacement
